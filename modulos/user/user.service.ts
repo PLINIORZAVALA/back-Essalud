754822910affe419c0b7e22ee3b0ad6be4d337a8
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-08-13 22:17:56 -0500

    sec commit

--- modulos/user/user.service.ts
+++ modulos/user/user.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { userEntity } from "src/entity/user.entity";
-import { Repository } from "typeorm";
-
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {userEntity} from 'src/entity/user.entity';
 
 @Injectable()
-export class userService {
-    constructor(@InjectRepository(userEntity) private userRepository: Repository<userEntity>) {}
+export class UserService {
 
-    findAll(): Promise<userEntity[]> {
-        return this.userRepository.find();
+    //Obtenemos los datos mediante el constructor
+    constructor(
+        @InjectRepository(userEntity)
+        private UserRepository: Repository<userEntity>,
+      ) {}
+
+      //Obtemos todos los datos del arreglo
+      findAll(): Promise<userEntity[]> {
+        return this.UserRepository.find();
       }
+
+    //Obtenemos los datos mediante el ID   
+    findOne(IdUser: number): Promise<userEntity> {
+        return this.UserRepository.findOne({ where: { IdUser } });
+    }
 }
