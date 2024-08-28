e93fab643d33ab6d899047870fc67862ecbff61f
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-08-28 12:56:59 -0500

    0.6 commit

--- modulos/user/user.controller.ts
+++ modulos/user/user.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import {UserService} from './user.service';
-<<<<<<< HEAD
-=======
+
 import {userEntity} from 'src/entity/user.entity';
->>>>>>> 7885bd9a3ceba7e59c31ee012c77f2216c76bf1d
 
 @Controller('user')
 export class UserController {
--- modulos/user/user.service.ts
+++ modulos/user/user.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import {userEntity} from 'src/entity/user.entity';
+import { promises } from 'dns';
+import { CreateUserDto } from 'src/dto/user/createUserDto.dto';
+import { updateUserDto } from 'src/dto/user/updateUserDto.dto';
 
 @Injectable()
 export class UserService {
@@ -20,5 +23,71 @@ export class UserService {
     //Obtenemos los datos mediante el ID   
     findOne(IdUser: number): Promise<userEntity> {
         return this.UserRepository.findOne({ where: { IdUser } });
+    }
+
+    //Insertamos datos  al base de datos
+    async insertUser(request: CreateUserDto) {
+      try {
+
+        const newUser = this.UserRepository.create({
+          FirstName : request.firstName,
+          LastName : request.firstName,
+          Mail : request.lastName,
+          Dni : request.dni,
+          Date : request.date
+        });
+  
+        // Guardar la nueva entidad de usuario en la base de datos
+        await this.UserRepository.save(newUser);
+  
+        return { msg: 'User inserted successfully', success: true };
+      } catch (error) {
+        console.error('Failed to insert user:', error);
+        return { msg: 'Failed to insert user', detailMsg: error, success: false };
+      }
+    }
+
+    //Actualizamos los datos de la base de datos
+    async updateUser(updateUserDto: updateUserDto) {
+      try {
+  
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        const user = await this.UserRepository.findOne({
+          where: { IdUser: updateUserDto.IdUser },
+        });
+        if (!user) {
+          return { msg: 'User not found', success: false };
+        }
+  
+        user.FirstName = updateUserDto.FirstName,
+        user.LastName = updateUserDto.LastName,
+        user.Password = updateUserDto.Password,
+        user.Dni = updateUserDto.Dni,
+        user.EmployeeCode = updateUserDto.EmployeeCode,
+        user.Shift = updateUserDto.Shift,
+        user.PhoneNumber = updateUserDto.PhoneNumber,
+        user.Mail = updateUserDto.Mail,
+        user.Rol = updateUserDto.Rol,
+        user.Date = updateUserDto.Date,
+        user.IndActive = updateUserDto.IndActive
+  
+        await this.UserRepository.save(user);
+  
+        return { msg: 'User updated successfully', success: true };
+      } catch (error) {
+        console.error('Failed to update user:', error);
+        return { msg: 'Failed to update user', detailMsg: error, success: false };
+      }
+    }
+
+    //Eliminamos datos de la base de datos
+    async deleteUser(userId: number) {
+      try {
+        await this.UserRepository.delete(userId);
+        return { msg: 'User deleted successfully', success: true };
+      } catch (error) {
+        console.error('Failed to delete user:', error);
+        return { msg: 'Failed to delete user', detailMsg: error, success: false };
+      }
     }
 }
